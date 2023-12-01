b4b9d28e67df5543b4d8916ac35dd7ff3db85791
Ewan Burnett <EwanBurnettSK@outlook.com>
2023-12-01 14:43:17 +0000

    Boilerplate

---
+++ 01-Trebuchet/main.cpp
@@ -0,0 +1,17 @@
+/*  Advent of Code 2023 Day 1 - Trebuchet
+ *  Given a list of strings, for each string extract
+ *  the 2 digit number consisting of the first and 
+ *  last digits in the string, then compute their sum.
+ *  ----------
+ *  Created: 01-12-23    Ewan Burnett
+ *  Revised: 01-12-23    Ewan Burnett
+ */
+
+#include <cstdio>
+#include <fstream>
+#include <string> 
+#include <vector> 
+
+int main(int argc, const char** argv) {
+
+}
