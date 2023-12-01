24d0806612387a1f98f0ff76289309e3210ee861
Ewan Burnett <EwanBurnettSK@outlook.com>
2023-12-01 15:59:38 +0000

    (Part 2) WIP - string comparisons - Logic works, strcmp doesn't. - Need to tidy up code.

--- 01-Trebuchet/main.cpp
+++ 01-Trebuchet/main.cpp
@@ -7,6 +7,8 @@
  *  Revised: 01-12-23    Ewan Burnett
  */
 
+#define PART_1 0
+
 #include <cstdio>
 #include <fstream>
 #include <string> 
@@ -31,14 +33,27 @@ void LoadInputData(const std::string& inFile, std::vector<std::string>& outData)
 
     std::string line;
     while (std::getline(file, line)) {
-        outData.push_back(line);
+        
+        //Convert the string to lowercase.
+        std::string lwr;
+        lwr.resize(line.length());
+        for (size_t i = 0; i < line.length(); i++) {
+            lwr[i] = std::tolower(line[i]);
+        }
+
+        outData.push_back(lwr);  
     }
 
     return;
 }
 
-
-uint64_t ComputeCalibrationValues(const std::vector<std::string>& data) {
+#if PART_1
+/*  Computes the sum Calibration Values from a given input data array
+ *  from the first and last integral values (0..9) of each element.
+ *  e.g. the string "treb7uchet" would give first = 7, last = 7, value = 77
+ *  e.g. the string "pqr3stu8vwx" would give first = 3, last = 8, value = 38
+ */
+uint64_t ComputeCalibrationValues_Part1(const std::vector<std::string>& data) {
 
     uint64_t acc = 0;
 
@@ -86,6 +101,183 @@ uint64_t ComputeCalibrationValues(const std::vector<std::string>& data) {
     return acc;
 }
 
+#else
+/*  Computes the sum Calibration Values from a given input data array
+ *  from the first and last numerical values of each element, including 
+ *  numbers spelled out in English.  
+ *  e.g. the string "eightwothree" would give first = 8, last = 3, value = 83
+ *  e.g. the string "zoneight234" would give first = 1, last = 4, value = 14
+  */
+uint64_t ComputeCalibrationValues_Part2(const std::vector<std::string>& data) {
+
+    uint64_t acc = 0;
+
+    for (const auto& str : data) {
+        std::string_view v{str};    //Acquire a view of the string we're processing. 
+
+        int8_t first = -1;
+        int8_t last = -1;
+
+        //crawl through the string, checking each element for an integer.
+        for (size_t i = 0; i < v.length(); i++) {
+            if (v[i] < '0') {
+                continue;
+            }
+            else if (v[i] > '9') {  //This might be a numerical string. 
+                if (v[i] < 'a') {
+                    continue;   //the character is either uppercase or a special character
+                }
+
+                //Compare the string with potential values. 
+               switch(v[i]){
+                case 'o':
+                    if (std::strcmp(&v[i], "one") == 0) {
+                        first = 1;
+                    break;
+                    }
+                    continue;
+                case 't':
+                    if (std::strcmp(&v[i], "two") == 0) {
+                        first = 2;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "three") == 0) {
+                        first = 3;
+                        break;
+                    }
+                    continue;
+                case 'f':
+                    if (std::strcmp(&v[i], "four") == 0) {
+                        first = 4;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "five") == 0) {
+                        first = 5;
+                    break;
+                    }
+                    continue;
+             case 's':
+                    if (std::strcmp(&v[i], "six") == 0) {
+                        first = 6;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "seven") == 0) {
+                        first = 7;
+                    break;
+                    }
+                    continue;
+             case 'e':
+                    if (std::strcmp(&v[i], "eight") == 0) {
+                        first = 8;
+                    break;
+                    }
+                    continue;
+             case 'n':
+                    if (std::strcmp(&v[i], "nine") == 0) {
+                        first = 9;
+                    break;
+                    }
+                    continue;
+                default:
+                    continue;
+                }
+
+                last = first;   //Assume the last int is equal to the first int, before the second pass. 
+                break;
+
+            }
+
+            else {  //This is an integer. 
+                first = v[i] - '0'; //Convert from ASCII into an int. 
+                last = first;   //Assume the last int is equal to the first int, before the second pass. 
+                break;
+            }
+        }
+
+        //Do the same, but backwards through the string.
+        for (size_t i = v.length() - 1; i > 0; i--) {
+            if (v[i] < '0') {
+                continue;   //Ignore special characters
+            }
+            else if (v[i] > '9') {  //This might be a numerical string. 
+                if (v[i] < 'a') {
+                    continue;   //the character is either uppercase or a special character
+                }
+
+                //Compare the string with potential values. 
+                switch(v[i]){
+                case 'o':
+                    if (std::strcmp(&v[i], "one") == 0) {
+                        last = 1;
+                    break;
+                    }
+                    continue;
+                case 't':
+                    if (std::strcmp(&v[i], "two") == 0) {
+                        last = 2;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "three") == 0) {
+                        last = 3;
+                        break;
+                    }
+                    continue;
+                case 'f':
+                    if (std::strcmp(&v[i], "four") == 0) {
+                        last = 4;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "five") == 0) {
+                        last = 5;
+                    break;
+                    }
+                    continue;
+             case 's':
+                    if (std::strcmp(&v[i], "six") == 0) {
+                        last = 6;
+                        break;
+                    }
+                    else if (std::strcmp(&v[i], "seven") == 0) {
+                        last = 7;
+                    break;
+                    }
+                    continue;
+             case 'e':
+                    if (std::strcmp(&v[i], "eight") == 0) {
+                        last = 8;
+                    break;
+                    }
+                    continue;
+             case 'n':
+                    if (std::strcmp(&v[i], "nine") == 0) {
+                        last = 9;
+                    break;
+                    }
+                    continue;
+                default:
+                    continue;
+                }
+
+                break;
+
+            }
+            else {
+                last = v[i] - '0'; //Convert from ASCII into an int. 
+                break;
+            }
+        }
+
+        if (first > -1 && last > -1) {   //Only add to the accumulator if a value has been found. 
+            //Promote the numbers to 2 digits, and add to the accumulator. 
+            auto cal = (first * 10) + last;
+            printf("%d\t%s\n", cal, v.data());
+            acc += cal;             }
+    }
+
+    return acc;
+}
+#endif
+
 int main(int argc, const char** argv) {
 
     printf("Recalibrating Values...\n");
@@ -100,7 +292,11 @@ int main(int argc, const char** argv) {
         LoadInputData(inputSource, g_InputData);
 
         //Compute the calibration value
-        uint64_t val = ComputeCalibrationValues(g_InputData);
+#if PART_1
+        uint64_t val = ComputeCalibrationValues_Part1(g_InputData);
+#else
+        uint64_t val = ComputeCalibrationValues_Part2(g_InputData);
+#endif
         printf("Recalibration Complete!\n>> %d", val);
 
 
